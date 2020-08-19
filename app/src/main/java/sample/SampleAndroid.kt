15922a2bfdc2ccfc12775e2e6a2ae7f2a7d1fdfb
Rocio Ortega <rocioortegapriego@gmail.com>
2020-08-19 16:23:44 +0200

    create mpp project supporting iOS and Android

---
+++ app/src/main/java/sample/SampleAndroid.kt
@@ -0,0 +1,17 @@
+package sample
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.jetbrains.handson.mpp.mobile.createApplicationScreenMessage
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
+    }
+}
---
+++ app/src/test/java/sample/SampleTestsAndroid.kt
@@ -0,0 +1,11 @@
+package sample
+
+import org.junit.Assert
+import org.junit.Test
+
+class SampleTestsAndroid {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
